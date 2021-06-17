59e0aed95cc9ad08ede65a6ba884a08f314c40f2
prog420 <dmitry.b.v.work@gmail.com>
2021-06-18 01:31:05 +0300

    Styles added (CSS Framework Bulma)

--- application/login/__init__.py
+++ application/login/__init__.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 from flask import current_app as app
+from application.models import User
 
 # Blueprint Configuration
 auth_bp = Blueprint(
